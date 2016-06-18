2126cb2129f71d0b250d94df54c573ea32147cfc
Tomasz Belina <tomasz.belina@qualent.eu>
2016-06-18 22:49:53 +0200

    Internal component communication - child/parent

--- app/components/about/about.ts
+++ app/components/about/about.ts
@@ -1,7 +1,32 @@
-import {Component} from 'angular2/core';
+import {Component, ViewChild, AfterViewInit} from 'angular2/core';
+
+@Component({
+    selector: 'hobby',
+    template: `<h3>Hobby</h3>`
+
+})
+class Hobby {
+    show(name) {
+        console.log(`Hello from ${name}.`);
+    }
+}
 
 @Component({
     selector: 'about',
-    template: `<h1>About Component</h1>`
+    template: `<h1>About Component</h1>
+    (exposing child api)
+    <hobby #hobby1></hobby>
+    <hobby #hobby2></hobby>
+
+    <button (click)="hobby2.show('Hobby 2')">Invoke show() on hobby 2</button>`,
+    directives: [Hobby]
 })
-export class AboutComponent {}
+export class AboutComponent implements AfterViewInit{
+
+	@ViewChild('hobby1')
+    firstHobby: Hobby;
+
+    ngAfterViewInit() {
+        this.firstHobby.show('Hobby 1');
+    }
+}
