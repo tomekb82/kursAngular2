f3659d43d0290e9e8625b658675a91ab3dec0a96
Tomasz Belina <tomasz.belina@qualent.eu>
2016-06-20 00:20:05 +0200

    Dodaj obsluge eydcji gwiazdek dla photo-detail

--- app/components/photo-detail/photo-detail.ts
+++ app/components/photo-detail/photo-detail.ts
@@ -1,4 +1,5 @@
 import {Component, ViewEncapsulation, bind, provide, Inject, Directive, Output, EventEmitter} from 'angular2/core';
+import {NgFor} from 'angular2/common';
 import {RouteConfig,  ROUTER_DIRECTIVES, RouteParams, RouteData} from 'angular2/router';
 import {Photo, Review, PhotoService, MockPhotoService} from '../../services/photo-service';
 import StarsComponent from '../stars/stars';
@@ -84,7 +85,7 @@ export default class PhotoParametersComponent {
             }, deps:["IS_DEV_ENVIRONMENT"]})],
   templateUrl: 'app/components/photo-detail/photo-detail.html',
   styles: ['photo-stars.large {font-size: 24px;} .env-prod {background: #286090; color: white;} .env-dev {background: red}'],
-  directives: [ROUTER_DIRECTIVES, PhotoDescriptionComponent, PhotoParametersComponent, StarsComponent]
+  directives: [ROUTER_DIRECTIVES, NgFor, PhotoDescriptionComponent, PhotoParametersComponent, StarsComponent]
 })
 @RouteConfig([
     {path: '/', component: PhotoParametersComponent , as: 'PhotoParameters'  },
@@ -96,9 +97,16 @@ export default class PhotoDetailComponent {
   type: string = "dev";
   showParams = false;
 
+  newComment: string;
+  newRating: number;
+
+  isReviewHidden: boolean = true;
+
+
   toggleValue = function(){
     this.showParams = !this.showParams;
   }
+
   constructor(params: RouteParams, data: RouteData, private photoService: PhotoService, @Inject('IS_DEV_ENVIRONMENT') private isDev: boolean){
     if(data.get('isProd')){
       this.type = "prod";
@@ -108,6 +116,27 @@ export default class PhotoDetailComponent {
     
     this.photo = photoService.getPhoto(photoId);
     this.reviews = photoService.getReviewsForPhoto(this.photo.id);
+  }
+
+
+  addReview() {
+    let review = new Review(0, this.photo.id, new Date(), 'Anonymous',
+        this.newRating, this.newComment);
+    this.reviews = [...this.reviews, review];
+    this.photo.rating = this.averageRating(this.reviews);
+
+    this.resetForm();
+  }
+
+  averageRating(reviews: Review[]) {
+    let sum = reviews.reduce((average, review) => average + review.rating, 0);
+    return sum / reviews.length;
+  }
+
+  resetForm() {
+    this.newRating = 0;
+    this.newComment = null;
+    this.isReviewHidden = true;
   }
 }
 
