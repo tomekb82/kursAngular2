59ad51f554c615f0e6b6d77adfb1e55471741b06
Tomasz Belina <tomasz.belina@qualent.eu>
2016-06-21 23:18:51 +0200

    Walidacja w formularzach

--- app/components/forms/forms.ts
+++ app/components/forms/forms.ts
@@ -5,16 +5,35 @@ import {
   ControlGroup,
   CORE_DIRECTIVES,
   FORM_DIRECTIVES,
-  NG_VALIDATORS
+  NG_VALIDATORS,
+  Validators
 } from 'angular2/common';
-
-
 
 function peselValidator(control: Control): {[key: string]: any} {
   const value: string = control.value || '';
   const valid = value.match(/^\d{11}$/);
   return valid ? null : {pesel: true};
 }
+
+/**
+ * Returns `true` if all Controls in the specified ControlGroup have exactly
+ * the same value. Otherwise returns `false`.
+ */
+function equalValidator({value}: ControlGroup): {[key: string]: any} {
+  const [first, ...rest] = Object.keys(value || {});
+  const valid = rest.every(v => value[v] === value[first]);
+  return valid ? null : {equal: true};
+}
+
+/**
+ * Wraps existing {@link equalValidator} function into a directive,
+ * so it can be used in a template to validate a form field.
+ */
+@Directive({
+  selector: '[equal]',
+  providers: [provide(NG_VALIDATORS, {useValue: equalValidator, multi: true})]
+})
+class EqualValidatorDirective {}
 
 
 function emailValidator(control: Control): {[key: string]: any} {
@@ -23,7 +42,10 @@ function emailValidator(control: Control): {[key: string]: any} {
   return valid ? null : {email: true};
 }
 
-
+/**
+ * Wraps existing {@link emailValidator} function into a directive,
+ * so it can be used in a template to validate a form field.
+ */
 @Directive({
   selector: '[email]',
   providers: [provide(NG_VALIDATORS, {useValue: emailValidator, multi: true})]
@@ -32,80 +54,46 @@ class EmailValidatorDirective {}
 
 @Component({
   selector: 'photo-form',
-  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, EmailValidatorDirective],
-  template: 
-  ` <h4>Template-driven:</h4>
-    <form #f="ngForm" (ngSubmit)="onSubmit(f.value)">
-      <div>Username:         <input type="text" ngControl="username"></div>
-      <div>
-      		Email:              <input type="text" ngControl="emailCtrl" email>
-      		<span [hidden]="!f.form.hasError('email', 'emailCtrl')">Email is invalid</span>
-      </div>
-      <div>Password:         <input type="password" ngControl="password"></div>
-      <div>Confirm password: <input type="password" ngControl="pconfirm"></div>
-      <button type="submit">Register</button>
-    </form>
-
-	<hr>
-    <h4>Data-driven:</h4>
-    <form [ngFormModel]="form" (ngSubmit)="register()">
-      <div>
-        <label for="username">Username</label>
-        <input id="username" type="text" ngControl="username">
-      </div>
-
-	  <div>
-        <label for="pesel">PESEL</label>
-      	<input id="pesel" type="text" ngControl="peselCtrl">
-        <span [hidden]="!form.hasError('pesel', 'peselCtrl')">PESEL in invalid</span>
-	  </div>
-
-      <div>
-        <label>Emails</label>
-        <ul ngControlGroup="emails">
-          <li *ngFor="#e of emails; #i = index">
-          	<input ngControl="{{i}}" email>
-          	
-          </li>
-        </ul>
-        <button type="button" (click)="addEmail()">Add Email</button>
-      </div>
-      <button type="submit">Register</button>
-    </form>
-    <label>Form Value:</label>
-    <pre>{{value}}</pre>
-
-  `
+  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, EmailValidatorDirective, EqualValidatorDirective],
+  templateUrl: 'app/components/forms/forms.html'
 })
 export default class FormsComponent {
-  onSubmit(formData) {
-    console.log(formData);
+  onSubmit(formValue: any, isFormValid: boolean) {
+    if (isFormValid) {
+      console.log(formValue);
+    }
   }
 
-  form: ControlGroup;
-  emails: Control[];
-  peselCtrl: Control;
+  formModel: ControlGroup;
+  emailsModel: Control[];
+  peselCtrlModel: Control;
 
   constructor() {
-    this.emails = [new Control('', emailValidator)];
+  	this.emailsModel = [new Control('', emailValidator)];
 
-    this.form = new ControlGroup({
-      username: new Control(),
-      emails: new ControlArray(this.emails),
-      peselCtrl: new Control('', peselValidator)
+    this.formModel = new ControlGroup({
+      'usernameModel': new Control('', Validators.required),
+      'emailsModel': new ControlArray(this.emailsModel),
+      'peselCtrlModel': new Control('', peselValidator),
+      'passwordsGroupModel': new ControlGroup({
+        'passwordModel': new Control('', Validators.minLength(5)),
+        'pconfirmModel': new Control('')
+      }, {}, equalValidator)
     });
   }
 
   get value() {
-    return JSON.stringify(this.form.value, null, 4);
+    return JSON.stringify(this.formModel.value, null, 4);
   }
 
   addEmail() {
-    const emails = <ControlArray>this.form.controls['emails'];
-    emails.push(new Control('', emailValidator));
+    const emailsModel = <ControlArray>this.formModel.controls['emails'];
+    emailsModel.push(new Control('', emailValidator));
   }
 
   register() {
-    console.log(this.form.value);
+    if (this.formModel.valid) {
+      console.log(this.formModel.value);
+    }
   }
 }
