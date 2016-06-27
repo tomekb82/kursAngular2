6fc1ff889b4ea3398aa95f4b5fdf91514dd0de7f
Tomasz Belina <tomasz.belina@qualent.eu>
2016-06-27 23:35:50 +0200

    Dodaj parametr metody

--- server/photo-rest-server.ts
+++ server/photo-rest-server.ts
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 app.get('/photos', (req, res) => {
-    res.json(getPhotos());
+    res.json(getPhotos(req.query));
 });
 app.get('/photos/:id', (req, res) => {
     res.json(getPhotoById(parseInt(req.params.id)));
