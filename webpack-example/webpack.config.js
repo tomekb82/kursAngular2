60b33cc19b4f8916fd5100a4b979de81b79459bc
Tomasz Belina <tomasz.belina@qualent.eu>
2016-07-07 22:23:11 +0200

    Dodaj pliki konfiguracyjne

--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -1,33 +1,51 @@
 const path               = require('path');
 const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 const CopyWebpackPlugin  = require('copy-webpack-plugin');
+const DefinePlugin       = require('webpack/lib/DefinePlugin');
+
+const ENV  = process.env.NODE_ENV = 'development';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 8080;
+
+const metadata = {
+  env    : ENV,
+  host   : HOST,
+  port   : PORT
+};
 
 module.exports = {
+  debug: true,
+  //watch: true,
+  devServer: {
+    contentBase: 'app',
+    historyApiFallback: true,
+    host: metadata.host,
+    port: metadata.port
+  },
+  devtool: 'source-map',
   entry: {
     'main'  : './app/main.ts',
     'vendor': './app/vendor.ts'
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/,  loader: 'raw', exclude: /node_modules/},
+      {test: /\.css$/,  loader: 'style!css?-minimize', exclude: /app/},
+      {test: /\.html$/, loader: 'raw'},
+      {test: /\.ts$/,   loader: 'ts', query: {compilerOptions: {noEmit: false}}}
+    ],
+    noParse: [path.join(__dirname, 'node_modules', 'angular2', 'bundles')]
   },
   output: {
     path    : './dist',
     filename: 'bundle.js'
   },
   plugins: [
-    new CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-    new CopyWebpackPlugin([{from: './app/index.html', to: 'index.html'}])
+    new CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity}),
+    new CopyWebpackPlugin([{from: './app/index.html', to: 'index.html'}]),
+    new DefinePlugin({'webpack': {'ENV': JSON.stringify(metadata.env)}})
   ],
   resolve: {
     extensions: ['', '.ts', '.js']
-  },
-  module: {
-    loaders: [
-      {test: /\.ts$/, loader: 'ts-loader'}
-    ],
-    noParse: [path.join(__dirname, 'node_modules', 'angular2', 'bundles')]
-  },
-  //watch: true,
-  devServer: {
-    contentBase: 'app',
-    historyApiFallback: true
-  },
-  devtool: 'source-map'
+  }
 };
