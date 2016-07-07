147e4a5f58c92a34de67806e6329a75ec732002b
Tomasz Belina <tomasz.belina@qualent.eu>
2016-07-07 23:32:40 +0200

    Popraw blad przy uruchamianiu aplikacji

--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -17,24 +17,24 @@ module.exports = {
   debug: true,
   //watch: true,
   devServer: {
-    contentBase: 'app',
+    contentBase: 'src',
     historyApiFallback: true,
     host: metadata.host,
     port: metadata.port
   },
   devtool: 'source-map',
   entry: {
-    'main'  : './app/main.ts',
-    'vendor': './app/vendor.ts'
+    'main'  : './src/main.ts',
+    'vendor': './src/vendor.ts'
   },
   module: {
     loaders: [
       {test: /\.css$/,  loader: 'raw', exclude: /node_modules/},
-      {test: /\.css$/,  loader: 'style!css?-minimize', exclude: /app/},
+      {test: /\.css$/,  loader: 'style!css?-minimize', exclude: /src/},
       {test: /\.html$/, loader: 'raw'},
       {test: /\.ts$/,   loader: 'ts', query: {compilerOptions: {noEmit: false}}}
     ],
-    noParse: [path.join(__dirname, 'node_modules', 'angular2', 'bundles')]
+    noParse: [path.join(__dirname, 'node_modules', 'angular2', 'bundles')]  
   },
   output: {
     path    : './dist',
@@ -42,10 +42,10 @@ module.exports = {
   },
   plugins: [
     new CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity}),
-    new CopyWebpackPlugin([{from: './app/index.html', to: 'index.html'}]),
+    new CopyWebpackPlugin([{from: './src/index.html', to: 'index.html'}]),
     new DefinePlugin({'webpack': {'ENV': JSON.stringify(metadata.env)}})
   ],
   resolve: {
-    extensions: ['', '.ts', '.js']
+    extensions: ['', '.ts', '.js', '.jsx']
   }
 };
